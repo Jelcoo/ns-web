e0e6ce36f4cb623a8fed170b4c9b23215bca949f
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2024-06-20 16:42:02 +0000

    Request cache

--- functions/ns_api.php
+++ functions/ns_api.php
@@ -26,19 +26,31 @@ function GetTrainTracksGeo() {
 }
 
 function GetTrainDisruptions() {
+    if (apcu_exists("current_disruptions")) {
+        return apcu_fetch("current_disruptions");
+    }
+
     $ch = InitCurl("https://gateway.apiportal.ns.nl/reisinformatie-api/api/v3/disruptions");
 
     $json = curl_exec($ch);
     $data = json_decode($json, true);
 
+    apcu_store("current_disruptions", $data, 60);
+
     return $data;
 }
 
 function GetTrainDisruptionsGeo() {
+    if (apcu_exists("current_disruptions_geo")) {
+        return apcu_fetch("current_disruptions_geo");
+    }
+
     $ch = InitCurl("https://gateway.apiportal.ns.nl/Spoorkaart-API/api/v1/storingen");
 
     $json = curl_exec($ch);
     $data = json_decode($json, true);
+
+    apcu_store("current_disruptions_geo", $data, 60);
 
     return $data;
 }
