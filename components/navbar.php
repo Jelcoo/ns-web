39707550b72fbae7e5ffa85a3deec1264505313e
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2024-07-04 19:48:53 +0000

    Remove console.log

--- components/navbar.php
+++ components/navbar.php
@@ -10,7 +10,6 @@
     const url = new URL(window.location.href);
     const path = url.pathname;
     navLinks.forEach(link => {
-        console.log(link.getAttribute('href'), path);
         if (link.getAttribute('href') === path) {
             link.classList.add('active');
         }
