553a91d8f8f65224438795eda6e5f85082a922e3
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2024-07-08 13:02:11 +0000

    Add train side display

--- components/trainmap_details.php
+++ components/trainmap_details.php
@@ -1,1 +1,18 @@
-<?php
+<div id="details">
+    <div class="detail">
+        <span class="detail_header">Ritnummer:</span>
+        <span id="detail_ritnr"></span>
+    </div>
+    <div class="detail">
+        <span class="detail_header">Snelheid:</span>
+        <span id="detail_speed"></span>
+    </div>
+    <div class="detail">
+        <span class="detail_header">Type:</span>
+        <span id="detail_type"></span>
+    </div>
+    <div class="detail">
+        <span class="detail_header">Materieel:</span>
+        <span id="detail_materieel"></span>
+    </div>
+</div>
