6a3fb3053dcb130b3c94b00e6f8f24ed25e344d0
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2024-07-11 18:53:28 +0000

    Don't fetch illegal map

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -1,7 +1,7 @@
 function makeMap(key, minZoom, maxZoom, tracks) {
     const map = L.map('map').setView([52.13580717626822, 5.746366037663725], 8);
 
-    L.tileLayer(`https://{s}.tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=${key}`, {
+    L.tileLayer(`https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}@2x.png`, {
         minZoom,
         maxZoom,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
